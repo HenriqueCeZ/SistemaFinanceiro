19778dcce30e61f7bac455581ea2d9d38a3a55ab
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-10-10 00:41:52 -0300

    Primeiro commit

---
+++ src/App.styles.ts
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+export const Container = styled.div `
+
+        color:black;
+
+
+
+`;
+
+export const Header = styled.div `
+        background-color:darkblue;
+        height:150px; 
+        text-align:center;
+        
+        
+
+
+
+
+`;
+export const Body = styled.div `
+
+        max-width:980px; 
+        margin: auto;
+        min-height: 600px;
+        margin-bottom: 50px;
+
+
+
+`;
+
+export const HeaderText = styled.h1 `
+
+        margin: 0;
+        padding:0;
+        color: #fff;
+        padding-top:30px;
+
+
+
+`;
---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import * as C from './App.styles'
+const App = () => {
+
+
+      return(
+        <div>
+        <C.Container>
+          <C.Header>
+
+            <C.HeaderText>Sistema Financeiro</C.HeaderText>
+
+          </C.Header>
+
+          <C.Body></C.Body>
+            
+
+        </C.Container>
+
+        </div>
+      );
+
+
+
+}
+
+
+export default App;
